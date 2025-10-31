4f722ea345a80a6007f913c2cf0a80ecc1aeb93a
Virendrapurohit <virendrasinghprohitw@gmail.com>
2025-10-31 13:57:46 +0530

    #update config file

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -40,7 +40,7 @@ const Sidebar: React.FC<{ isDarkTheme: boolean }> = ({ isDarkTheme }) => {
       >
         <img
           style={{ height: 52, cursor: "pointer" }}
-          src={"/CapsiTask.png"}
+          src={"/admin-dashboard/CapsiTask.png"}
           alt="Logo"
         />
       </div>
